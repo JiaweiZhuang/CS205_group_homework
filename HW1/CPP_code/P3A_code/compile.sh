876a477a750a8baae0cfdc44419ff01db7268177
Jiawei Zhuang <jiaweizhuang@g.harvard.edu>
2017-02-26 10:49:50 -0500

    turn on compiler optimization
    It turns out that -O2 yields better performance than -O3

--- HW1/CPP_code/P3A_code/compile.sh
+++ HW1/CPP_code/P3A_code/compile.sh
@@ -2,4 +2,4 @@
 
 # c++11 standard is required for the chrono (timing) library 
 # need g++-5 or newer
-g++ vector_sum.cpp -o vector_sum.out -std=c++11 -fopenmp -lpthread
+g++ -O2 vector_sum.cpp -o vector_sum.out -std=c++11 -fopenmp -lpthread
