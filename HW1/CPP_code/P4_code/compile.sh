f87f99e3c46e0e59956f5d36b0a865929c1f91a1
js8383 <jeisen8383@gmail.com>
2017-02-27 19:20:53 -0500

    fix linux openblas issue, now testing cuda

--- HW1/CPP_code/P4_code/compile.sh
+++ HW1/CPP_code/P4_code/compile.sh
@@ -3,5 +3,11 @@
 # c++11 standard is required for the chrono (timing) library 
 # need g++-5 or newer
 
-g++ -O2 MxM.cpp -o MxM.out -std=c++11 -fopenmp -lpthread lib/libopenblas.a
+if [ "$(uname)" == "Darwin" ]; then
+	g++ -O2 MxM.cpp -o MxM.out -std=c++11 -fopenmp -lpthread lib/libopenblas_mac.a
+else
+	g++ -O2 MxM.cpp -o MxM.out -std=c++11 -fopenmp -lpthread lib/libopenblas_linux.a
+fi 
+	
+
 # g++ -O2 MxM.cpp -o MxM.out -std=c++11 # use this for serial build
