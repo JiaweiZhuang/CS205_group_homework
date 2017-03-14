55537eba365b0d02ea83e19a219850002f76b685
Xinyuan Wang <xwang146@binghamton.edu>
2017-03-14 17:12:07 -0400

    wrote sequential APSP Floyd-Warshall algorithm

---
+++ HW2/P4_APSP/APSP_Floyd.c
@@ -0,0 +1,56 @@
+# include <stdlib.h>
+# include <stdio.h>
+# include <math.h>
+# define INF 100
+# define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+int** Make2DIntArray(int arraySizeX, int arraySizeY) {
+    int** theArray;
+    theArray = (int**) malloc(arraySizeX*sizeof(int*));
+    for (int i = 0; i < arraySizeX; i++)
+        theArray[i] = (int*) malloc(arraySizeY*sizeof(int));
+    return theArray;
+} 
+
+
+int main(){
+	//build the graph of lecture11 slide23
+	int N; //number of vertex;
+	int i, j, k;
+
+	N=4;
+
+    int** D = Make2DIntArray(N, N);	
+	for (i=0; i<N; i++){
+    	for (j=0; j<N; j++){
+        D[i][j]=INF;
+    	}
+    }
+
+    for (i=0; i<N; i++){
+    	D[i][i] = 0;
+    }
+
+    //build the edges
+    D[0][2]=3;
+    D[1][0]=2;
+    D[2][1]=7;
+    D[2][3]=1;
+    D[3][0]=6;
+
+    for(k=0; k<N; k++)
+    	for(i=0; i<N; i++)
+    		for(j=0; j<N; j++){
+    			D[i][j] = MIN(D[i][j], D[i][k]+D[k][j]);
+    		}
+
+    //check
+	for (i=0; i<N; i++){
+    	for (j=0; j<N; j++){
+        printf("%d ", D[i][j]);
+    	}
+    	printf("\n");
+    }
+
+
+}
